1cf460a6db1622c9f77d9b4953dae71cbac49495
Tanmay Upreti <iamtanmayupreti@gmail.com>
2024-06-07 07:13:42 +0530

    String practice questions

---
+++ practice/string_questions.py
@@ -0,0 +1,11 @@
+# STRING PRACTICE QUESTIONS
+print('# STRING PRACTICE QUESTIONS')
+
+# 1. WAPP to input a an integer and check if it contains 0 in it.
+print('# 1. WAPP to input a an integer and check if it contains 0 in it.')
+n = int(input('Enter the integer: '))
+n = str(n)
+if '0' in n:
+    print('Yes, there is 0 in',n)
+else:
+    print('No, there is no 0 in',n)
