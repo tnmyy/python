52e067827828931502b3b726b6072b0b550159bd
Tanmay Upreti <iamtanmayupreti@gmail.com>
2024-06-11 07:22:16 +0530

    Conditional and Iterative statements' questions

---
+++ practice/conditional_iterative_questions.py
@@ -0,0 +1,49 @@
+# Conditional and Iterative Statement Questions
+print('# Conditional and Iterative Statement Questions')
+
+# 1. WAPP to add all the odd numbers upto N(included)
+print('#1. Add all the odd numbers')
+N = int(input('Enter a number to be added: '))
+total = 0
+i = 1
+while i<=N:
+    total+=i
+    i += 2
+print('The sum of all the odd numbers till',N,'is: ',total)
+    
+# 2. WAPP to find the factorial of a N
+N = int(input('Enter a number to be added: '))
+fct = 1
+i = 1
+while i<N:
+    fct *= i
+    i+=1
+print('The factoral of',N,'is: ', fct)
+
+# 3. WAPP to input 3 numbers from the user and display the maximum and the minimum number among them
+n1 = int(input('Enter the first number: '))
+n2 = int(input('Enter the second number: '))
+n3 = int(input('Enter the third number: '))
+
+max_num = min_num = None
+
+if n1>n2 and n1>n3:
+    if n2>n3:
+        max_num = n1
+        min_num = n3
+    else:
+        min_num = n2
+elif n2>n1 and n2>n3:
+    if n1>n3:
+        max_num = n1
+        min_num = n3
+    else:
+        min_num = n1
+elif n3>n1 and n3>n2:
+    if n1>n2:
+        max_num = n3
+        min_num = n2
+    else:
+        min_num = n1
+print('Largest number: ', max_num)
+print('Smallest number: ', min_num)
