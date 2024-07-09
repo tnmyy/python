e43ff609fc2c742c642d74f69179aec3ad8e6bf0
Tanmay Upreti <iamtanmayupreti@gmail.com>
2024-07-09 19:10:01 +0530

    String Questions

--- practice/string_questions.py
+++ practice/string_questions.py
@@ -13,10 +13,38 @@ print('# BASED ON PYTHON STRING OPERATORS')
 ##    print('No, there is no 0 in',n)
 
 # 2. WAPP which asks the user to enter the username and a code. Check that username is not present in code
-print('# 2. WAPP which asks the user to enter the username and a code. Check that username is not present in code')
+'''print('# 2. WAPP which asks the user to enter the username and a code. Check that username is not present in code')
 user_name = input("Enter the user name: ")
 code = input("Enter the code: ")
 if user_name not in code:
     print('The user name: ', user_name, '\nThe code:', code)
 else:
-    print("You have entered the user name in your code!")
+    print("You have entered the user name in your code!")'''
+
+# 3. WAPP to find the number of vowels, consonants, numbers, spaces in a string
+print('# 3. WAPP to find the number of vowels, consonants, numbers, spaces in a string')
+vow = 'aeiou'
+conso = 'bcdfghjklmnpqrstvwxyz'
+num = '0123456789'
+spc = ' '
+string = input('Enter your string: ')
+string = string.lower()
+
+vow_count = conso_count = num_count = spc_count = special_count = 0
+for i in range(len(string)):
+    if string[i] in vow:
+        vow_count+=1
+    elif string[i] in conso:
+        conso_count +=1
+    elif string[i] in num:
+        num_count +=1
+    elif string[i] in spc:
+        spc_count += 1
+    else:
+        special_count+=1
+
+print('Vowels: ', vow_count)
+print('Consonants: ', conso_count)
+print('Numbers: ', num_count)
+print('Spaces: ', spc_count)
+print('Special Characters: ', special_count)
