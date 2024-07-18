c6e15f6566d3993c9ac878d8d126a1e5dda9f550
Tanmay Upreti <iamtanmayupreti@gmail.com>
2024-07-18 20:04:56 +0530

    String Methods

--- strings/string_functions_methods.py
+++ strings/string_functions_methods.py
@@ -4,7 +4,7 @@ print('# STRING FUNCTIONS AND METHODS')
 # 1. len() function
 # returns the length of a strings
 print('# 1. len() function')
-a = 'Tanmay upreti'
+a = 'Tanmay Upreti'
 print(len(a))
 print(len('Python is an impressive language'))
 
@@ -19,7 +19,7 @@ print('i love my india'.capitalize())
 # returns the number of times a specified value appears in a string
 # SYNTAX: string.count(sub_str, start_index, end_index)
 print('# 3. count() method')
-a = 'Waake up a to a reality'
+a = 'Wake up a to a reality'
 print(a.count('a'))
 print(a[:3])
 print(a.count('a', 0, 3))
@@ -47,4 +47,110 @@ print(a.index(b))
 b = 'little  '
 # print(a.index(b)) it thows error
 
+# 6. isalnum() method
+# returns True if the characters of a string are alphanumeric (alphabets and numbers). If at least one character is non-alphanumeric, it returns False
+# ' ' (empty string) is not considered as alphanumeric
+print('# 6. isalnum() method')
+a = 'abc'
+b = '123'
+c = 'Hey Bro'
+print(a.isalnum())
+print(b.isalnum())
+print(c.isalnum())
 
+# 7. isalpha() method
+# Returns True only if there are alphabets in a string, else False
+print('# 7. isalpha() method')
+a = 'abc'
+b = '123'
+c = 'Hey Bro'
+print(a.isalpha())
+print(b.isalpha())
+print(c.isalpha())
+
+# 8. isdigit() method
+# Returns True is all the characters in a string are digits, else False
+print('# 8. isdigit() method')
+a = 'abc'
+b = '123'
+c = 'Hey Bro'
+print(a.isdigit())
+print(b.isdigit())
+print(c.isdigit())
+
+# 9. islower()
+# Returns True is all the string characters are in lower case
+print('# 9. islower()')
+a = 'abc'
+b = ' a'
+c = '123'
+print(a.islower())
+print(b.islower())
+print(c.islower())
+
+# 10. isspace()
+# Returns True if there are only whitespaces in a string. And False if there is at least 1 characters
+print('# 10. isspace()')
+a = '   '
+b = ''
+c = 'H '
+print(a.isspace())
+print(b.isspace())
+print(c.isspace())
+
+# 11. isupper()
+# Returns True is all the string characters are in upper case
+print('# 11. isupper()')
+a = 'ABC'
+b = ' A'
+c = '123'
+print(a.isupper())
+print(b.isupper())
+print(c.isupper())
+
+# 12. lower()
+# Converts all the alphabets of a string to lowercase
+print('# 12. lower()')
+a = 'Hey EvEryOne'
+b = 'Hey EverBodY 123'
+print(a.lower())
+print(b.lower())
+
+# 13. upper()
+# Converts all the alphabets of a string to uppercase
+print('# 13. upper()')
+a = 'Hey EvEryOne'
+b = 'Hey EverBodY 123'
+print(a.upper())
+print(b.upper())
+
+# 14.1 lstrip()
+# Removes all the leading whitespaces in a string(Left most)
+print('# 14.1 lstrip()')
+a = '       Hey'
+b = '   Hey     '
+print(a.lstrip())
+print(len(a.lstrip()))
+print(b.lstrip())
+print(len(b.lstrip()))
+
+# 14.2 rstrip()
+# Removes all the trailing whitespaces in a string(Right most)
+print('# 14.2 rstrip()')
+a = 'Hey    '
+b = '   Hey     '
+print(a.rstrip())
+print(len(a.rstrip()))
+print(b.rstrip())
+print(len(b.rstrip()))
+
+# 14.3 strip()
+# Removes whitespaces of both LEADING and TRAILING sides
+print('# 14.3 strip()')
+a = '   Hey     '
+print(a.strip())
+print(len(a.strip()))
+
+# 15.1 startswith()
+# Returns True if a given string starts with a given substring
+print('# 15.1 startswith()')
