dc7882e5e17b7dbb32eb3598bae6a173b1e73045
Tanmay Upreti <iamtanmayupreti@gmail.com>
2024-11-19 06:52:23 +0530

    Tuple Operations

--- tuple/tuple_operations.py
+++ tuple/tuple_operations.py
@@ -17,3 +17,14 @@ print(t1 * n)
 # Slicing
 t1 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 print(t1[::2])
+
+# PACKING OF TUPLES
+# creating a tuple from a set of values is called packing. therefore, creating individual elements from the tuple's elements is called unpacking
+
+t = (1, 2, True, "Hey")
+print(t)
+a, b, c, d = t  # number of variables == number of elements
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
