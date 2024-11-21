508c804bf2f9867220a1af9481128e3a26582f34
Tanmay Upreti <iamtanmayupreti@gmail.com>
2024-11-21 22:17:00 +0530

    Dictionary Introduction

---
+++ dictionary/dict_intro.py
@@ -0,0 +1,21 @@
+# DICTIONARIES = Key-Value Pairs
+# MUTABLE
+# AKA Associative arrays, Mappings or Hashes
+
+# Making a dictionary
+
+# Empty Dictionary
+d = dict()
+print(d, type(d))
+# OR
+d = {}
+print(d, type(d))
+
+# SYNTAX: {key: value, key: value}
+# key = immutable type i.e. string, number or tuple(with immutable entries)
+
+d = {"key": "value", 1: "value1"}
+print(d)
+
+# d = {[1,2]:'value'} ==> Give ERROR
+# d = {"key": "value", "key": "value1"}
