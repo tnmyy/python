19de8a180b0ffe0a60e17ac50a189d594c3cf4b0
Tanmay Upreti <iamtanmayupreti@gmail.com>
2024-12-04 07:08:58 +0530

    Dictionary baiscs

--- dictionary/dict_basics.py
+++ dictionary/dict_basics.py
@@ -13,3 +13,29 @@ emp = {"name": "John", "salary": 150, "male": True}
 print(emp)
 emp["salary"] = 69
 print(emp)
+
+# Deleting elements from a dictionary
+emp = {"name": "John", "salary": 150, "male": True}
+print(emp)
+del emp["name"]
+print(emp)
+
+# del emp["name"] # key error
+del emp
+# print(emp) # emp is not defined now
+
+# Checking for the existence of a key in a dictionary
+emp = {"name": "John", "salary": 150, "male": True}
+print("job" in emp)  # only checks for keys
+print("name" in emp)
+print("age" not in emp)
+
+# Pretty printing a dictionary
+# just to make it more readable
+name = ("John", "Jacob", "Donald", "Georgia", "Joe", "Ben")
+rank = (1, 2, 3, 4, 6, 5)
+dict_rank = dict(zip(name, rank))
+import json
+
+print(dict_rank)
+print(json.dumps(dict_rank, indent=2))
