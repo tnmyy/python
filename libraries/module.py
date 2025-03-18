9811154cfdccebf7a2573de7750b8ea52554422a
Tanmay Upreti <iamtanmayupreti@gmail.com>
2025-03-18 22:17:21 +0530

    Libraries and Modules

---
+++ libraries/module.py
@@ -0,0 +1,20 @@
+# IMPORTING MODULE
+import random
+
+print(random.random())
+
+import statistics as s
+
+print(s.mean((1, 2, 3, 4, 5)))
+
+
+# is tareke se module.functionName karne ki jarurat nahi
+from math import sin, cos, sqrt
+
+print(sin(0), cos(0))
+
+from math import *
+
+print(pow(3, 4))
+print(floor(-2.3))
+print(factorial(8))
